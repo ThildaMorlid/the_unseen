841ca0e7786583606f7723671a1c5da71a98bcd4
ThildaMorlid <thildaberglindmorlid@gmail.com>
2024-02-04 20:19:09 +0100

    påbörjar appen

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+// Lägger in Bootstrap CSS stilar
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+// Importera Bootstrap JS filer
+
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +18,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// för att import ska fungera så behöver vi anv dessa med ett middleware "use"
+
+app.use(bootstrap)
 
 app.mount('#app')
